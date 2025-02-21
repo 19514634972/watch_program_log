aa9c84d8fb081514cd39abf8bdeb424a3aabb8b1
wzf <1490216271@qq.com>
2025-02-21 20:27:52 +0800

    fix-log

---
+++ target/debug/deps/log-5e62f08d1a5c6c69.d
@@ -0,0 +1,10 @@
+D:\rust_code\watch_program_log\target\debug\deps\liblog-5e62f08d1a5c6c69.rmeta: C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\lib.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\macros.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\serde.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\__private_api.rs
+
+D:\rust_code\watch_program_log\target\debug\deps\liblog-5e62f08d1a5c6c69.rlib: C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\lib.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\macros.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\serde.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\__private_api.rs
+
+D:\rust_code\watch_program_log\target\debug\deps\log-5e62f08d1a5c6c69.d: C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\lib.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\macros.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\serde.rs C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\__private_api.rs
+
+C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\lib.rs:
+C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\macros.rs:
+C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\serde.rs:
+C:\Users\wezone\.cargo\registry\src\index.crates.io-6f17d22bba15001f\log-0.4.26\src\__private_api.rs:
---
+++ target/debug/deps/watch_script-ce19b5a8c7a58208.d
@@ -0,0 +1,12 @@
+D:\rust_code\watch_program_log\target\debug\deps\libwatch_script-ce19b5a8c7a58208.rmeta: watch_script\src\lib.rs watch_script\src\config\mod.rs watch_script\src\config\log.rs watch_script\src\initialize\mod.rs watch_script\src\initialize\config.rs watch_script\src\initialize\kafka.rs
+
+D:\rust_code\watch_program_log\target\debug\deps\libwatch_script-ce19b5a8c7a58208.rlib: watch_script\src\lib.rs watch_script\src\config\mod.rs watch_script\src\config\log.rs watch_script\src\initialize\mod.rs watch_script\src\initialize\config.rs watch_script\src\initialize\kafka.rs
+
+D:\rust_code\watch_program_log\target\debug\deps\watch_script-ce19b5a8c7a58208.d: watch_script\src\lib.rs watch_script\src\config\mod.rs watch_script\src\config\log.rs watch_script\src\initialize\mod.rs watch_script\src\initialize\config.rs watch_script\src\initialize\kafka.rs
+
+watch_script\src\lib.rs:
+watch_script\src\config\mod.rs:
+watch_script\src\config\log.rs:
+watch_script\src\initialize\mod.rs:
+watch_script\src\initialize\config.rs:
+watch_script\src\initialize\kafka.rs:
